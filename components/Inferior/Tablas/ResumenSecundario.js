d2ef45e715ac8506bdedb59b90a8d60b642e11a7
KANG-27 <85331451+KANG-27@users.noreply.github.com>
2023-11-09 02:39:57 -0500

    agrege grafica de barras tipo hurto

--- components/Inferior/Tablas/ResumenSecundario.js
+++ components/Inferior/Tablas/ResumenSecundario.js
@@ -1,6 +1,14 @@
 import React from "react";
 import LocalidadesV2 from "json/LocalidadesV2.json";
-import { VictoryBar, VictoryChart, VictoryPie, VictoryTooltip } from "victory";
+import {
+  Bar,
+  VictoryArea,
+  VictoryBar,
+  VictoryChart,
+  VictoryLabel,
+  VictoryPie,
+  VictoryTooltip,
+} from "victory";
 
 export const ResumenSecundario = ({ añoSelect, localidad, candidatura }) => {
   let candidaturaCludia;
@@ -10,41 +18,94 @@ export const ResumenSecundario = ({ añoSelect, localidad, candidatura }) => {
     let prueba = LocalidadesV2.CandidaturaDos.años.filter(
       (x) => x.añoCandidatura == añoSelect[0].añoCandidatura
     );
-    let pruebaDos = prueba[0].centrosEnLocalidades.filter((x)=>x.LOCALIDADES==localidad)
+    let pruebaDos = prueba[0].centrosEnLocalidades.filter(
+      (x) => x.LOCALIDADES == localidad
+    );
     candidaturaCludia = pruebaDos[0];
-    let quitararray = añoSelect[0].centrosEnLocalidades.filter((x)=>x.LOCALIDADES==localidad);
-    candidaturaEnrique = quitararray[0]
+    let quitararray = añoSelect[0].centrosEnLocalidades.filter(
+      (x) => x.LOCALIDADES == localidad
+    );
+    candidaturaEnrique = quitararray[0];
   } else {
     let prueba = LocalidadesV2.CandidaturaUno.años.filter(
       (x) => x.añoCandidatura == añoSelect[0].añoCandidatura
     );
-    let pruebaDos = prueba[0].centrosEnLocalidades.filter((x)=>x.LOCALIDADES==localidad)
+    let pruebaDos = prueba[0].centrosEnLocalidades.filter(
+      (x) => x.LOCALIDADES == localidad
+    );
     candidaturaEnrique = pruebaDos[0];
-    let quitararray = añoSelect[0].centrosEnLocalidades.filter((x)=>x.LOCALIDADES==localidad);
-    candidaturaCludia = quitararray[0]
+    let quitararray = añoSelect[0].centrosEnLocalidades.filter(
+      (x) => x.LOCALIDADES == localidad
+    );
+    candidaturaCludia = quitararray[0];
   }
 
-
-  console.log(candidaturaEnrique)
-  console.log(candidaturaCludia)
-
   let resumenPie = [
-    { x: candidaturaEnrique.LOCALIDADES, y: candidaturaEnrique.Total, u:"Enrique"},
-    { x: candidaturaCludia.LOCALIDADES, y:candidaturaCludia.Total, u:"Claudia" },
+    {
+      x: candidaturaEnrique.LOCALIDADES,
+      y: candidaturaEnrique.Total,
+      u: "Enrique",
+    },
+    {
+      x: candidaturaCludia.LOCALIDADES,
+      y: candidaturaCludia.Total,
+      u: "Claudia",
+    },
   ];
+
+  let resumenBarras = {
+    candidaturaEnrique: [
+      {
+        quarter: "ArmaBlanca",
+        earnings: parseInt(candidaturaEnrique.ArmaBlanca),
+      },
+      { quarter: "ArmaFuego", earnings: parseInt(candidaturaCludia.ArmaFuego) },
+      {
+        quarter: "Contundentes",
+        earnings: parseInt(candidaturaEnrique.Contundentes),
+      },
+      {
+        quarter: "Escopolamina",
+        earnings: parseInt(candidaturaEnrique.Escopolamina),
+      },
+      {
+        quarter: "NoReportados",
+        earnings: parseInt(candidaturaEnrique.NoReportados),
+      },
+      { quarter: "SinArmas", earnings: parseInt(candidaturaEnrique.SinArmas) },
+    ],
+    candidaturaClaudia: [
+      {
+        quarter: "ArmaBlanca",
+        earnings: parseInt(candidaturaCludia.ArmaBlanca),
+      },
+      { quarter: "ArmaFuego", earnings: parseInt(candidaturaCludia.ArmaFuego) },
+      {
+        quarter: "Contundentes",
+        earnings: parseInt(candidaturaCludia.Contundentes),
+      },
+      {
+        quarter: "Escopolamina",
+        earnings: parseInt(candidaturaCludia.Escopolamina),
+      },
+      {
+        quarter: "NoReportados",
+        earnings: parseInt(candidaturaCludia.NoReportados),
+      },
+      { quarter: "SinArmas", earnings: parseInt(candidaturaCludia.SinArmas) },
+    ],
+  };
 
   return (
     <>
-    {/* <span>claudia = {}</span> */}
-      <div className="mx-96">
+      {/* <span>claudia = {}</span> */}
+      <div className="mx-96 flex flex-col">
         <VictoryPie
-        height={300}
           data={resumenPie}
-          colorScale={[
-            "grey",
-            "darkcyan",
-          ]}
-          labels={({ datum }) => `Año candidatura ${añoSelect[0].añoCandidatura} de ${datum.u} Total de Robos: ${datum.y}`}
+          colorScale={["grey", "darkcyan"]}
+          labels={({ datum }) =>
+            `Año candidatura ${añoSelect[0].añoCandidatura} de ${datum.u} Total de Robos: ${datum.y}`
+          }
           labelComponent={<VictoryTooltip />}
           style={{
             labels: {
@@ -52,6 +113,31 @@ export const ResumenSecundario = ({ añoSelect, localidad, candidatura }) => {
             },
           }}
         />
+        <div className="flex flex-col">
+          <VictoryChart domainPadding={50}>
+            <VictoryBar
+              horizontal
+              height={200} width={300}
+              data={resumenBarras.candidaturaEnrique}
+              x="quarter"
+              y="earnings"
+              labels={({ datum }) => ` ${datum.earnings}`}
+              textAnchor="middle"
+        style={{ fontSize: 18, fill: 'black' }}
+            />
+          </VictoryChart>
+          <VictoryChart domainPadding={40}>
+            <VictoryBar
+              horizontal
+              data={resumenBarras.candidaturaClaudia}
+              x="quarter"
+              y="earnings"
+              labels={({ datum }) => ` ${datum.earnings}`}
+              textAnchor="middle"
+              
+            />
+          </VictoryChart>
+        </div>
       </div>
     </>
   );
