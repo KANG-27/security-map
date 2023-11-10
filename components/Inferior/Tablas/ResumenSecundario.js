ae40355c6a016ca6d972ff8fc5e31222fc821670
KANG-27 <85331451+KANG-27@users.noreply.github.com>
2023-11-09 22:08:57 -0500

    cambio de estilos en resumensecundario y agregue cantidad de cais y hospitales

--- components/Inferior/Tablas/ResumenSecundario.js
+++ components/Inferior/Tablas/ResumenSecundario.js
@@ -100,32 +100,63 @@ export const ResumenSecundario = ({ añoSelect, localidad, candidatura }) => {
     <>
       {/* <span>claudia = {}</span> */}
       <div className="mx-96 flex flex-col">
-        <VictoryPie
-          data={resumenPie}
-          colorScale={["grey", "darkcyan"]}
-          labels={({ datum }) =>
-            `Año candidatura ${añoSelect[0].añoCandidatura} de ${datum.u} Total de Robos: ${datum.y}`
-          }
-          labelComponent={<VictoryTooltip />}
-          style={{
-            labels: {
-              fontSize: 6,
-            },
-          }}
-        />
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
+          <span>año candidatura:</span>
+          <span>{añoSelect[0].añoCandidatura}</span>
+        </div>
+        <div className="pie-total-hurtos-select">
+          <span>Total de hurtos en las distintas alcaldias</span>
+          <div className="flex justify-center">
+            <VictoryPie
+              data={resumenPie}
+              colorScale={["grey", "darkcyan"]}
+              labels={({ datum }) =>
+                `Año candidatura ${añoSelect[0].añoCandidatura} de ${datum.u} Total de Robos: ${datum.y}`
+              }
+              labelComponent={<VictoryTooltip />}
+              style={{
+                labels: {
+                  fontSize: 12,
+                },
+              }}
+            />
+          </div>
+
+          <div className="flex justify-around mt-10">
+            <div className="flex flex-col items-center">
+              <span>Cai candidatura Enrique</span>
+              <span>{candidaturaEnrique.cai.length}</span>
+            </div>
+            <div className="flex flex-col  items-center">
+              <span>Cai candidatura Claudia</span>
+              <span>{candidaturaCludia.cai.length}</span>
+            </div>
+          </div>
+          <div className="flex justify-around mb-10">
+            <div className="flex flex-col  items-center">
+              <span>Hospitales candidatura Enrique</span>
+              <span>{candidaturaEnrique.hospital.length}</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span>Hospitales candidatura claudia</span>
+              <span>{candidaturaCludia.hospital.length}</span>
+            </div>
+          </div>
+        </div>
+        <span>Tipo de hurtos</span>
+        <div className="flex w-full barras-laterales-selected">
           <VictoryChart domainPadding={50}>
             <VictoryBar
               horizontal
-              height={200} width={300}
               data={resumenBarras.candidaturaEnrique}
               x="quarter"
               y="earnings"
               labels={({ datum }) => ` ${datum.earnings}`}
               textAnchor="middle"
-        style={{ fontSize: 18, fill: 'black' }}
+              style={{ fontSize: 18, fill: "black" }}
             />
           </VictoryChart>
+
           <VictoryChart domainPadding={40}>
             <VictoryBar
               horizontal
@@ -134,7 +165,6 @@ export const ResumenSecundario = ({ añoSelect, localidad, candidatura }) => {
               y="earnings"
               labels={({ datum }) => ` ${datum.earnings}`}
               textAnchor="middle"
-              
             />
           </VictoryChart>
         </div>
