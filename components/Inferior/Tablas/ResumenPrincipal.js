358f6d72324dded42541aa06cdf48f0fd9e3161f
OscarMendoza22 <oscarluis.mendoza@gmail.com>
2023-11-14 20:06:01 -0500

    Graficas_Finales_resumenPrincipal

--- components/Inferior/Tablas/ResumenPrincipal.js
+++ components/Inferior/Tablas/ResumenPrincipal.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import LocalidadesV2 from "json/LocalidadesV2.json";
-import { Bar, VictoryBar, VictoryChart, VictoryLabel, VictoryPie, VictoryTooltip, } from 'victory';
-export const ResumenPrincipal = (años, locali, candi) => {
+import { Bar, VictoryArea, VictoryAxis, VictoryBar, VictoryChart, VictoryClipContainer, VictoryGroup, VictoryLabel, VictoryLegend, VictoryLine, VictoryStack, VictoryTooltip, } from 'victory';
+import App from '@/pages/_app';
+export const ResumenPrincipal = () => {
+  console.log(LocalidadesV2.CandidaturaUno.años[0].totalCais);
+  const data1 = [
+    { x: LocalidadesV2.CandidaturaUno.años[0].año, y: LocalidadesV2.CandidaturaUno.años[0].totalCais },
+    { x: LocalidadesV2.CandidaturaUno.años[1].año, y: LocalidadesV2.CandidaturaUno.años[1].totalCais },
+    { x: LocalidadesV2.CandidaturaUno.años[2].año, y: LocalidadesV2.CandidaturaUno.años[2].totalCais },
+    { x: LocalidadesV2.CandidaturaUno.años[3].año, y: LocalidadesV2.CandidaturaUno.años[3].totalCais },
+    { x: LocalidadesV2.CandidaturaDos.años[0].año, y: LocalidadesV2.CandidaturaDos.años[0].totalCais },
+    { x: LocalidadesV2.CandidaturaDos.años[1].año, y: LocalidadesV2.CandidaturaDos.años[1].totalCais },
+    { x: LocalidadesV2.CandidaturaDos.años[2].año, y: LocalidadesV2.CandidaturaDos.años[2].totalCais },
+    { x: LocalidadesV2.CandidaturaDos.años[3].año, y: LocalidadesV2.CandidaturaDos.años[3].totalCais },
+  ]
+  const data2 = [
+    { x: LocalidadesV2.CandidaturaUno.años[0].año, y: LocalidadesV2.CandidaturaUno.años[3].totalhospitales },
+    { x: LocalidadesV2.CandidaturaUno.años[1].año, y: LocalidadesV2.CandidaturaUno.años[2].totalhospitales },
+    { x: LocalidadesV2.CandidaturaUno.años[2].año, y: LocalidadesV2.CandidaturaUno.años[2].totalhospitales },
+    { x: LocalidadesV2.CandidaturaUno.años[3].año, y: LocalidadesV2.CandidaturaUno.años[3].totalhospitales },
+    { x: LocalidadesV2.CandidaturaDos.años[0].año, y: LocalidadesV2.CandidaturaDos.años[0].totalhospitales },
+    { x: LocalidadesV2.CandidaturaDos.años[1].año, y: LocalidadesV2.CandidaturaDos.años[1].totalhospitales },
+    { x: LocalidadesV2.CandidaturaDos.años[2].año, y: LocalidadesV2.CandidaturaDos.años[2].totalhospitales },
+    { x: LocalidadesV2.CandidaturaDos.años[3].año, y: LocalidadesV2.CandidaturaDos.años[3].totalhospitales },
+  ]
   return (
-    <div className='flex flex-col p-5 items-center gap-6'> 
-      <span className='text-xl'>Total de cais en la alcaldia de:</span>
+    <div className='flex flex-col p-5 items-center gap-6'>
+      <span className='text-xl'>Total de cai en la alcaldia de:</span>
       <div className='flex items-center'>
         <div className='flex flex-col items-center'>
           <span className='font-bold text-base'>Enrique Peñalosa</span>
@@ -50,6 +72,74 @@ export const ResumenPrincipal = (años, locali, candi) => {
             />
           </VictoryChart>
         </div>
+        <div className='flex flex-col items-center'>
+        <span>Hospitales XD</span>
+          <span className='font-bold text-base'>Enrique Peñalosa</span>
+          <VictoryChart domainPadding={{ x: 40, y: 40 }}>
+            <VictoryBar
+              style={{ data: { fill: "#000000" } }}
+              data={[
+                { x: new Date(LocalidadesV2.CandidaturaUno.años[0].año, 2, 2), y: LocalidadesV2.CandidaturaUno.años[0].totalhospitales },
+                { x: new Date(LocalidadesV2.CandidaturaUno.años[1].año, 2, 2), y: LocalidadesV2.CandidaturaUno.años[1].totalhospitales },
+                { x: new Date(LocalidadesV2.CandidaturaUno.años[2].año, 2, 2), y: LocalidadesV2.CandidaturaUno.años[2].totalhospitales },
+                { x: new Date(LocalidadesV2.CandidaturaUno.años[3].año, 2, 2), y: LocalidadesV2.CandidaturaUno.años[3].totalhospitales }
+              ]}
+              labels={({ datum }) => `CAI: ${datum.y}`}
+              labelComponent={<VictoryTooltip />}
+              dataComponent={
+                <Bar
+                  tabIndex={0}
+                  ariaLabel={({ datum }) => `x: ${datum.x}`}
+                />
+              }
+            />
+          </VictoryChart>
+        </div>
+        <div className='flex flex-col items-center'>
+        <span>Hospitales XD</span>
+          <span className='font-bold text-base'>Enrique Peñalosa</span>
+          <VictoryChart domainPadding={{ x: 40, y: 40 }}>
+            <VictoryBar
+              style={{ data: { fill: "#000000" } }}
+              data={[
+                { x: new Date(LocalidadesV2.CandidaturaDos.años[0].año, 2, 2), y: LocalidadesV2.CandidaturaDos.años[0].totalhospitales },
+                { x: new Date(LocalidadesV2.CandidaturaDos.años[1].año, 2, 2), y: LocalidadesV2.CandidaturaDos.años[1].totalhospitales },
+                { x: new Date(LocalidadesV2.CandidaturaDos.años[2].año, 2, 2), y: LocalidadesV2.CandidaturaDos.años[2].totalhospitales },
+                { x: new Date(LocalidadesV2.CandidaturaDos.años[3].año, 2, 2), y: LocalidadesV2.CandidaturaDos.años[3].totalhospitales }
+              ]}
+              labels={({ datum }) => `CAI: ${datum.y}`}
+              labelComponent={<VictoryTooltip />}
+              dataComponent={
+                <Bar
+                  tabIndex={0}
+                  ariaLabel={({ datum }) => `x: ${datum.x}`}
+                />
+              }
+            />
+          </VictoryChart>
+        </div>  
+      </div>
+      <div>
+        <span className='text-xl'>Total en la alcaldia de:</span>
+        <VictoryChart domainPadding={20}>
+          <VictoryLegend
+            x={150}
+            y={0}
+            title="Comparación"
+            centerTitle
+            orientation="horizontal"
+            gutter={20}
+            style={{ border: { stroke: 'black' }, title: { fontSize: 20 } }}
+            data={[
+              { name: 'CAI', symbol: { fill: 'black' } },
+              { name: 'Hospitales', symbol: { fill: 'white' } },
+            ]}
+          />
+          <VictoryAxis tickValues={[LocalidadesV2.CandidaturaUno.años[0].año,LocalidadesV2.CandidaturaUno.años[1].año, LocalidadesV2.CandidaturaUno.años[2].año, LocalidadesV2.CandidaturaUno.años[3].año, LocalidadesV2.CandidaturaDos.años[0].año,LocalidadesV2.CandidaturaDos.años[1].año,LocalidadesV2.CandidaturaDos.años[2].año,LocalidadesV2.CandidaturaDos.años[3].año ]} />
+          <VictoryAxis dependentAxis />
+          <VictoryBar data={data1} x="x" y="y" style={{ data: { fill: 'black' } }} />
+          <VictoryBar data={data2} x="x" y="y" style={{ data: { fill: 'white' } }} />
+        </VictoryChart>
       </div>
     </div>
   )
